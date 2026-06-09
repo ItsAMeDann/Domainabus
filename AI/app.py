f08dd8163134e2b75228cdd0b8725dca3dc4c468
Hamdan <hamdan24001@mail.unpad.ac.id>
2026-06-09 13:12:34 +0700

    fix: deployment now accept CORS
    Cross-Origin Resource Sharing (CORS) now works to accept request from itch.io

--- AI/app.py
+++ AI/app.py
@@ -1,9 +1,11 @@
 import os
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/tes-api', methods=['GET'])
 def check_status():
